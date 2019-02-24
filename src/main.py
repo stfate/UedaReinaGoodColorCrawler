dd6231c2fbd4f0fa3d7d83b0a4bef48e736b0dce
stfate <stfate@gmail.com>
2019-02-24 09:43:41 +0900

    reflect un-saved changes

--- src/main.py
+++ src/main.py
@@ -17,15 +17,9 @@ if __name__ == "__main__":
 
     articles = good_color_crawler.get_page_urls()
 
-<<<<<<< HEAD:src/good_color_crawler.py
     # bar = progressbar.ProgressBar()
     # for _article in bar(articles):
         # good_color_crawler.download_images(_article)
 
     good_color_crawler.download_images(articles[0])
     
-=======
-    bar = progressbar.ProgressBar()
-    for _article in bar(articles):
-        good_color_crawler.download_article(_article, output_dir)
->>>>>>> 2207ea905b946fe51f5f84c5e3d4baea540bcb6c:src/main.py
